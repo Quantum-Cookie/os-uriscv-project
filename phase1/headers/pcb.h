208073f2fc684036788c80e42a72278ebd1ed2c8
cookie <quantum.cookie42@gmail.com>
2026-01-05 20:47:52 +0100

    docs: aggiunto commenti stile doxygen a funzioni parte 2

--- phase1/headers/pcb.h
+++ phase1/headers/pcb.h
@@ -24,27 +24,91 @@ void mkEmptyProcQ(struct list_head* head);
 int emptyProcQ(struct list_head* head);
 
 // Insert PCB "p" in the list "head"
+/**
+ * @brief Inserisce il PCB puntato da p nella coda dei processi puntato da head
+ * mantenendo la coda ordinata per priorita' decrescente.
+ * Se ci fossero piu' PCB con la stessa priorita' p va inserito dopo di essi.
+ * 
+ * @param head Elemento sentinella della coda dei processi
+ * @param p Puntatore al PCB da inserire
+ * 
+ * @note @p head e @p p devono essere puntatori validi (non NULL)
+ */
 void insertProcQ(struct list_head* head, pcb_t* p);
 
 // Return the first PCB in the list "head" without removing it
 pcb_t* headProcQ(struct list_head* head);
 
 // Remove and return the first PCB in the list "head"
+/**
+ * @brief Rimuove e restituisce il primo PCB nella lista puntato da @p head
+ * 
+ * @param head Elemento sentinella della coda dei processi
+ * @return `pcb_t*` Puntatore al primo PCB rimosso, o NULL se la coda era vuota
+ * 
+ * @note @p head deve essere un puntatore valido (non NULL)
+ */
 pcb_t* removeProcQ(struct list_head* head);
 
 // Remove PCB "p" from the list "head"
+/**
+ * @brief Rimuove il PCB puntato da @p p dalla coda dei processi puntato da @p head
+ * 
+ * @param head Elemento sentinella della coda dei processi
+ * @param p Puntatore al PCB da rimuovere dalla coda
+ * @return `pcb_t*` Puntatore al PCB rimosso, o NULL se il PCB puntato da @p p
+ * non appartiene alla lista @p head 
+ * 
+ * @note @p head e @p p devono essere puntatori validi (non NULL)
+ */
 pcb_t* outProcQ(struct list_head* head, pcb_t* p);
 
 // Check if the PCB "p" has children
+/**
+ * @brief Controlla se il PCB @p p e' privo di figli
+ * 
+ * @param p PCB di cui controllare se ha figli
+ * @return int `1` (True) se non ha figli, `0` (False) se ha figli
+ * 
+ * @note @p p deve essere un puntatore valido (non NULL)
+ */
 int emptyChild(pcb_t* p);
 
 // Insert the PCB child "p" into the PCB parent "prnt"
+/**
+ * @brief Rende il PCB puntato @p p figlio del PCB puntato da @p prnt 
+ * 
+ * @param prnt Puntatore al PCB padre
+ * @param p Puntatore PCB figlio da inserire
+ * 
+ * @note @p prnt e @p p devono essere puntatori validi (non NULL)
+ */
 void insertChild(pcb_t* prnt, pcb_t* p);
 
 // Remove and return the first child of the PCB "p"
+/**
+ * @brief Rimuove e restituisce il primo figlio del PCB puntato da @p p
+ * 
+ * @param p Puntatore al PCB da cui rimuovere il primo figlio
+ * @return `pcb_t*` Puntatore al figlio rimosso, o NULL se non ha figli
+ * 
+ * @note Il campo `p_parent` del figlio rimosso diventa NULL
+ * 
+ * @note @p p deve essere un puntatore valido (non NULL)
+ */
 pcb_t* removeChild(pcb_t* p);
 
 // Remove and return the PCB "p" from the parent's children list
+/**
+ * @brief Rimuove il PCB puntato da @p p dalla lista dei figli del padre (se presente)
+ * 
+ * @param p Puntatore del PCB di cui bisogna rimuovere la relazione di parentela
+ * @return `pcb_t*` @p p, o NULL se non ha padre 
+ * 
+ * @note Il campo `p_parent` di @p p diventa NULL
+ * 
+ * @note @p p deve essere un puntatore valido (non NULL)
+ */
 pcb_t* outChild(pcb_t* p);
 
 #endif
