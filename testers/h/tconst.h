c2e31dff5d51d774f01cb080873f37c0cb71e89d
cookie <quantum.cookie42@gmail.com>
2026-06-28 16:52:52 +0200

    feat: Implementato Shell

--- testers/h/tconst.h
+++ testers/h/tconst.h
@@ -7,3 +7,13 @@
 #define WRITETERMINAL 4
 #define READTERMINAL  5
 #define EXECUTE       6
+
+#define ASID_FIBEIGHT 2
+#define ASID_ECHO 3
+#define ASID_FIBELEVEN 4
+#define ASID_UNAME 5
+#define ASID_DATE 6
+#define ASID_SL 7
+#define ASID_CALC 8
+
+#define EXIT "exit"
